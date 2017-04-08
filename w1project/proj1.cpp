3a7272354b9c3523a718167c0531a639bee9e841
entegral <e2robby@gmail.com>
2017-04-08 11:08:15 -0700

    fixed OR operator

--- w1project/proj1.cpp
+++ w1project/proj1.cpp
@@ -27,27 +27,27 @@ int main()
 	cout << "\nWelcome to SmartCart." << endl;
 	while (!checkoutStatus)
 	{
-		//cout << "Please enter the name of the item you put into your cart:" << endl;
-		//cin.get(newItem, MAX_CHAR, '\n');
-		//cin.ignore(MAX_CHAR, '\n');
+		cout << "Please enter the name of the item you put into your cart:" << endl;
+		cin.get(newItem, MAX_CHAR, '\n');
+		cin.ignore(MAX_CHAR, '\n');
 
-		//cout << "How many " << newItem << "(s) did you put into your cart?" << endl;
-		//cin >> quantity;
-		//cin.ignore(MAX_CHAR, '\n');
-////
-		//cout << "How much do(es) " << newItem << " cost?" << endl;
-		//cin >> newItemPrice;
-		//cin.ignore(MAX_CHAR, '\n');
+		cout << "How many " << newItem << "(s) did you put into your cart?" << endl;
+		cin >> quantity;
+		cin.ignore(MAX_CHAR, '\n');
 
-		//cartSubtotal = cartSubtotal + (quantity * newItemPrice);
-		//cout << "New SmartCart Subtotal: " << cartSubtotal << endl;
-		//cout << "You have " << counter++ << "items in your cart. \nAre you ready to checkout? (y/n)" << endl;
+		cout << "How much do(es) " << newItem << " cost?" << endl;
+		cin >> newItemPrice;
+		cin.ignore(MAX_CHAR, '\n');
+
+		cartSubtotal = cartSubtotal + (quantity * newItemPrice);
+		cout << "New SmartCart Subtotal: " << cartSubtotal << endl;
+		cout << "You have " << ++counter << " items in your cart. \nAre you ready to checkout? (y/n)" << endl;
 		cin >> prompt;
 		cin.ignore(MAX_CHAR, '\n');
 
-		if (prompt == 'y' || 'Y')
+		if (prompt == 'y' || prompt == 'Y')
 			checkoutStatus = true;
 	}
-	cout << "Your Grand Total is: $" << cartSubtotal << " and you have " << counter << " item in your cart.\n" << endl;
+	cout << "Your Grand Total is: $" << cartSubtotal << " and you have " << counter << " items in your cart.\n" << endl;
 	return 0;
 }
