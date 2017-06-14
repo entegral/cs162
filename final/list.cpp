a1e28cafb556dbf96a617fae6cb8924a51060162
entegral <e2robby@gmail.com>
2017-06-14 12:19:12 -0700

    finding working bit

--- final/list.cpp
+++ final/list.cpp
@@ -5,44 +5,46 @@ using namespace std;
 
 void removeEven(node * &head)
 {
-	node * current, * prior, * next;
-	current = head;
-	while (current)
-        {
-		prior = current;
-		current = current->next;
-		next = current->next;
+	// removal from three positions: at head, middle, or at tail
+	node *current = head;
+	node * prior = NULL;
+	node * next = NULL;
 
-		// if at head
-    		if (head->data % 2 == 0)
-    		{
-    		   	node *spare = head;
-   				head = head->next;
-   			    delete spare;
-   			    spare = NULL;
-   			    continue;
-			}
-
-			// if current is tail
-			else if (current->data % 2 == 0 && !current->next)
+		while (current)
 			{
-				prior->next = NULL;
-				delete current;
-				continue;
+				prior = current;
+				current = current->next;
+		
+				// if at head
+				if (head->data % 2 == 0)
+				{
+					node *spare = head;
+					head = head->next;
+					delete spare;
+					spare = NULL;
 				}
-			// if current is not tail
-			else if (current->data % 2 ==0)
-			{
-				prior->next = current->next;
-				delete current;
-				current = prior;
-				continue;
-			}
-			else
-			{
-				continue;
-			}
-		}	    
+
+				// if current is not tail
+				else if (current->data % 2 == 0)
+				{
+					prior->next = current->next;
+					delete current;
+					current = prior->next;
+					continue;
+				}
+
+				// if current is tail
+				else if (current->data % 2 == 0 && !current->next)
+				{
+					prior->next = NULL;
+					delete current;
+					continue;
+				}
+				else
+				{
+					continue;
+				}
+			}    
 }
 	
 
