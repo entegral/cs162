405233468ea006594dc8e70b0d2be9378a10ef7f
entegral <e2robby@gmail.com>
2017-06-14 10:24:48 -0700

    bookmark

--- final/list.cpp
+++ final/list.cpp
@@ -11,39 +11,39 @@ void removeEven(node * &head)
 	node * next = NULL;
 
 		while (current)
-	        {
-			prior = current;
-			current = current->next;
-			next = current->next;
-	
-			// if at head
-        		if (head->data % 2 == 0)
-        		{
-        		   	node *spare = head;
-       				head = head->next;
-       			        delete spare;
-       			        spare = NULL;
-			}
-
-			// if current is tail
-			else if (current->data % 2 == 0 && !current->next)
 			{
-				prior->next = NULL;
-				delete next;
-				continue;
+				prior = current;
+				current = current->next;
+				next = current->next;
+		
+				// if at head
+				if (head->data % 2 == 0)
+				{
+					node *spare = head;
+					head = head->next;
+					delete spare;
+					spare = NULL;
 				}
-			// if current is not tail
-			else if (current->data % 2 ==0)
-			{
-				prior->next = current->next;
-				delete next;
-				continue;
-			}
-			else
-			{
-				continue;
-			}
-	        }    
+
+				// if current is tail
+				else if (current->data % 2 == 0 && !current->next)
+				{
+					prior->next = NULL;
+					delete next;
+					continue;
+				}
+				// if current is not tail
+				else if (current->data % 2 == 0)
+				{
+					prior->next = current->next;
+					delete next;
+					continue;
+				}
+				else
+				{
+					continue;
+				}
+			}    
 }
 
 int countEven(node * head)
