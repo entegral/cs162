917ce34c6a599e2d214cf7631f7efbdcb4a67c7e
entegral <e2robby@gmail.com>
2017-06-14 12:17:12 -0700

    another attempt

--- final/list.cpp
+++ final/list.cpp
@@ -6,6 +6,7 @@ using namespace std;
 void removeEven(node * &head)
 {
 	node * current, * prior, * next;
+	current = head;
 	while (current)
         {
 		prior = current;
@@ -17,29 +18,31 @@ void removeEven(node * &head)
     		{
     		   	node *spare = head;
    				head = head->next;
-   			        delete spare;
-   			        spare = NULL;
-		}
-
-		// if current is tail
-		else if (current->data % 2 == 0 && !current->next)
-		{
-			prior->next = NULL;
-			delete next;
-			continue;
+   			    delete spare;
+   			    spare = NULL;
+   			    continue;
 			}
-		// if current is not tail
-		else if (current->data % 2 ==0)
-		{
-			prior->next = current->next;
-			delete next;
-			continue;
-		}
-		else
-		{
-			continue;
-		}
-        }    
+
+			// if current is tail
+			else if (current->data % 2 == 0 && !current->next)
+			{
+				prior->next = NULL;
+				delete current;
+				continue;
+				}
+			// if current is not tail
+			else if (current->data % 2 ==0)
+			{
+				prior->next = current->next;
+				delete current;
+				current = prior;
+				continue;
+			}
+			else
+			{
+				continue;
+			}
+		}	    
 }
 	
 
